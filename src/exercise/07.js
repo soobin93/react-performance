a8b555569106bbe28996ad98e1328794557382da
Soobin Son <ssbin93@gmail.com>
2023-02-27 11:17:43 +1000

    do exercise 07

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,35 +1,31 @@
 // Production performance monitoring
 // http://localhost:3000/isolated/exercise/07.js
 
-import * as React from 'react'
-// 🐨 you're going to need the reportProfile function
-// 💰 here, let me help you with that...
-// import reportProfile from '../report-profile'
+import React, { Profiler, useState } from 'react';
+import reportProfile from 'report-profile';
 
-function Counter() {
-  const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>{count}</button>
-}
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(c => c + 1);
+  return <button onClick={increment}>{count}</button>;
+};
 
-function App() {
+const App = () => {
   return (
     <div>
-      {/*
-      🐨 Wrap this div in a React.Profiler component
-      give it the ID of "counter" and pass reportProfile
-      to the onRender prop.
-      */}
-      <div>
-        Profiled counter
-        <Counter />
-      </div>
+      <Profiler id="counter" onRender={reportProfile}>
+        <div>
+          Profiled counter
+          <Counter />
+        </div>
+      </Profiler>
+
       <div>
         Unprofiled counter
         <Counter />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
